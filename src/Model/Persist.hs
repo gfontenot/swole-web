462cf995930b0438e808b6a9ba5a12d329352b1e
Gordon Fontenot <gmoney@gitpushgitpaid.com>
2017-07-16 11:57:42 -0500

    Add initial model structure
    This will serve as the first draft of our model structure. This might change
    over time once we start using it, but it feels like a reasonable start.

--- src/Model/Persist.hs
+++ src/Model/Persist.hs
@@ -5,6 +5,8 @@ module Model.Persist where
 import ClassyPrelude.Yesod
 import Database.Persist.Quasi
 
+import Model.Weight
+
 -- You can define all of your database entities in the entities file.
 -- You can find more information on persistent and how to declare entities
 -- at:

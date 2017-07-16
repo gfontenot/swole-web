aa77d8876506b2e4f4bf9f99e82f0dd35fdac6d4
Gordon Fontenot <gmoney@gitpushgitpaid.com>
2017-07-16 11:43:00 -0500

    Break out persistence into new module
    This will make it easier to break out model specific modules moving forward.

--- src/Model.hs
+++ src/Model.hs
@@ -1,13 +1,5 @@
-{-# LANGUAGE FlexibleInstances #-}
+module Model
+    ( module Model
+    ) where
 
-module Model where
-
-import ClassyPrelude.Yesod
-import Database.Persist.Quasi
-
--- You can define all of your database entities in the entities file.
--- You can find more information on persistent and how to declare entities
--- at:
--- http://www.yesodweb.com/book/persistent/
-share [mkPersist sqlSettings, mkMigrate "migrateAll"]
-    $(persistFileWith lowerCaseSettings "config/models")
+import Model.Persist as Model

b56b40907f3ee42a21db840a5e8321cbb4fb1f19
Gordon Fontenot <gmoney@gitpushgitpaid.com>
2017-09-18 08:30:59 -0500

    Add List helper for enumerating lists
    This function takes in a list and returns an enumerated version of that list.

---
+++ elm/Helpers/List.elm
@@ -0,0 +1,11 @@
+module Helpers.List exposing (enumerated)
+
+import List.Extra exposing (zip)
+
+enumerated : List a -> List (Int, a)
+enumerated xs =
+    let
+        length = List.length xs
+        range = List.range 0 length
+    in
+       zip range xs

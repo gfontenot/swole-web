937ef844e091bd72f256f37b0a7eba0ad24525d2
Gordon Fontenot <gmoney@gitpushgitpaid.com>
2018-02-19 11:20:31 -0600

    Add movements manually via button click
    I want to let users type + to add a new movement, but I _also_ want to let
    them click a button to manually add a new movement to the complex. The former
    behavior exists for speed, the latter is for discoverability.

--- elm/Swole/Components/MovementInput.elm
+++ elm/Swole/Components/MovementInput.elm
@@ -1,15 +1,16 @@
 module Swole.Components.MovementInput exposing
     ( main )
 
-import Html exposing (Html, beginnerProgram, div, input, text)
+import Html exposing (Html, beginnerProgram, div, input, text, button)
 import Html.Attributes exposing (placeholder, type_, value)
-import Html.Events exposing (onInput)
+import Html.Events exposing (onClick, onInput)
 
 type alias Movement = String
 type alias Movements = List Movement
 
 type Msg
     = MovementChanged Movement
+    | AddMovement
 
 view : Movements -> Html Msg
 view movements =
@@ -19,13 +20,25 @@ view movements =
 update : Msg -> Movements -> Movements
 update msg movements =
     case msg of
-        MovementChanged m -> splitMovements m
+        MovementChanged m ->
+            splitMovements m
+
+        AddMovement ->
+            movements ++ [""]
 
 movementFields : Movements -> List (Html Msg)
 movementFields movements = case movements of
     [] -> [movementField ""]
     ms ->
-        List.intersperse (text "+") (List.map movementField ms)
+        let fields = List.intersperse plusLabel (List.map movementField ms)
+        in
+           fields ++ [plusButton]
+
+plusLabel : Html Msg
+plusLabel = text "+"
+
+plusButton : Html Msg
+plusButton = button [ onClick AddMovement ] [ text "+" ]
 
 movementField : Movement -> Html Msg
 movementField movement =

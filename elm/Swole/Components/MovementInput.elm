2a3ffb7026d09419b002442176e292cae48515bb
Gordon Fontenot <gmoney@gitpushgitpaid.com>
2018-02-20 21:02:45 -0600

    Track movement index
    Previously, when a movement changed, we were just returning that movement
    again. That's not actually what we want, though. Instead, in order to handle
    changes properly, we need to be able to track which movement changed and edit
    it directly.
    
    By tracking the index, we're also able to do things like insert a new set in
    the middle of the list by typing `+`.

--- elm/Swole/Components/MovementInput.elm
+++ elm/Swole/Components/MovementInput.elm
@@ -4,12 +4,15 @@ module Swole.Components.MovementInput exposing
 import Html exposing (Html, beginnerProgram, div, input, text, button)
 import Html.Attributes exposing (placeholder, type_, value)
 import Html.Events exposing (onClick, onInput)
+import List.Extra as List
+
+import Helpers.List as List
 
 type alias Movement = String
-type alias Movements = List Movement
+type alias Movements = List (Int, Movement)
 
 type Msg
-    = MovementChanged Movement
+    = MovementChanged (Int, Movement)
     | AddMovement
 
 view : Movements -> Html Msg
@@ -20,15 +23,30 @@ view movements =
 update : Msg -> Movements -> Movements
 update msg movements =
     case msg of
-        MovementChanged m ->
-            splitMovements m
+        MovementChanged (idx, m) ->
+            let
+                newMovements = splitMovements m
+                (before, after) = movementsAround idx movements
+                allMovements = before ++ newMovements ++ after
+            in
+                List.enumerated allMovements
 
         AddMovement ->
-            movements ++ [""]
+            let
+                rawMovements = List.map Tuple.second movements
+            in
+                List.enumerated <| rawMovements ++ [""]
+
+movementsAround : Int -> Movements -> (List Movement, List Movement)
+movementsAround idx movements
+    = movements
+    |> List.map Tuple.second
+    |> List.splitAt idx
+    |> Tuple.mapSecond (List.drop 1)
 
 movementFields : Movements -> List (Html Msg)
 movementFields movements = case movements of
-    [] -> [movementField ""]
+    [] -> [movementField (0, "")]
     ms ->
         let fields = List.intersperse plusLabel (List.map movementField ms)
         in
@@ -40,13 +58,13 @@ plusLabel = text "+"
 plusButton : Html Msg
 plusButton = button [ onClick AddMovement ] [ text "+" ]
 
-movementField : Movement -> Html Msg
-movementField movement =
+movementField : (Int, Movement) -> Html Msg
+movementField (idx, movement) =
     input
         [ type_ "text"
         , placeholder "movement"
         , value <| movement
-        , onInput MovementChanged
+        , onInput (\m -> MovementChanged (idx, m))
         ]
         []
 
